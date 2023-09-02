84fee52703e977c0681deebcd8a32de6033f7968
Marko Dodik <marko.dodik21@gmail.com>
2023-09-02 19:21:41 +0200

    minimum size subarray sum

--- src/main/java/org/example/pattern1/Solution.java
+++ src/main/java/org/example/pattern1/Solution.java
@@ -43,4 +43,31 @@ public class Solution {
         }
         return sum / k;
     }
+
+    /**
+     *  https://leetcode.com/problems/minimum-size-subarray-sum/
+     *
+     *  209. Minimum Size Subarray Sum
+     *
+     * */
+
+    public int minSubArrayLen(int target, int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+
+        int i = 0;
+        int j = 0;
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+
+        while (j < nums.length) {
+            sum += nums[j++];
+
+            while (sum >= target) {
+                min = Math.min(min, j - i);
+                sum -= nums[i++];
+            }
+        }
+
+        return min == Integer.MAX_VALUE ? 0 : min;
+    }
 }
