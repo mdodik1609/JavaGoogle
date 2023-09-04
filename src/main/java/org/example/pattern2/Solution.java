c45c53947ac70dba384a7c57a0201728e407b9d5
Marko Dodik <marko.dodik21@gmail.com>
2023-09-04 22:19:06 +0200

    squares of a sorted array

--- src/main/java/org/example/pattern2/Solution.java
+++ src/main/java/org/example/pattern2/Solution.java
@@ -6,13 +6,13 @@ import java.util.Hashtable;
 
 public class Solution {
     public static void main(String[] args){
-        Arrays.stream(twoSum(new int[]{2, 7, 11, 15}, 9)).forEach( it -> System.out.print(it));
-        System.out.println("\n====================");
-        Arrays.stream(twoSum(new int[]{3,3}, 6)).forEach( it -> System.out.print(it));
-        System.out.println("\n====================");
-        Arrays.stream(twoSum(new int[]{3,2,4}, 6)).forEach( it -> System.out.print(it));
+//        Arrays.stream(twoSum(new int[]{2, 7, 11, 15}, 9)).forEach( it -> System.out.print(it));
+//        System.out.println("\n====================");
+//        Arrays.stream(twoSum(new int[]{3,3}, 6)).forEach( it -> System.out.print(it));
+//        System.out.println("\n====================");
+//        Arrays.stream(twoSum(new int[]{3,2,4}, 6)).forEach( it -> System.out.print(it));
 
-
+        Arrays.stream(sortedSquares(new int[]{-7,-3,2,3,11})).forEach( it -> System.out.print(it + ","));
     }
 
     /**
@@ -32,5 +32,32 @@ public class Solution {
             table.put(nums[i], i);
         }
         throw new IllegalArgumentException("Should have found solution.");
+    }
+
+    /**
+     *  https://leetcode.com/problems/squares-of-a-sorted-array/
+     *
+     *  977. Squares of a Sorted Array
+     * */
+    public static int[] sortedSquares(int[] nums) {
+        int l = nums.length;
+        int[] result = new int[l];
+
+        int endPointer = l - 1;
+        int lastPlace = l - 1;
+
+        for(int startPointer = 0; startPointer < l; startPointer++) {
+            if(Math.pow(nums[startPointer], 2) < Math.pow(nums[endPointer], 2) ) {
+                result[lastPlace] = (int) Math.pow(nums[endPointer], 2);
+                endPointer--;
+                startPointer--;
+            } else {
+                result[lastPlace] = (int) Math.pow(nums[startPointer], 2);
+            }
+            lastPlace--;
+
+            if(startPointer == endPointer) break;
+        }
+        return result;
     }
 }
