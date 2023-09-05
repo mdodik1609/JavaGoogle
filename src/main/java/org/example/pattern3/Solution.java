df6f6e8e2d18b187fd32df7c999a1b137100baf9
Marko Dodik <marko.dodik21@gmail.com>
2023-09-05 11:09:28 +0200

    linked list cycle

--- src/main/java/org/example/pattern3/Solution.java
+++ src/main/java/org/example/pattern3/Solution.java
@@ -8,7 +8,49 @@ package org.example.pattern3;
  * */
 public class Solution {
     public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+
+        System.out.println(hasCycle(head));
+
+        head.next.next.next.next.next.next = head.next.next;
+        System.out.println(hasCycle(head));
+
 
     }
 
+
+    /**
+     *  https://leetcode.com/problems/linked-list-cycle/
+     *
+     *  141. Linked List Cycle
+     * */
+    public static boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(slow == fast) return true;
+        }
+        return false;
+    }
 }
+
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+   }
+}
+
