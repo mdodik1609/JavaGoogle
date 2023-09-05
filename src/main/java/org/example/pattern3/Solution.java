c24c9fe28fdb4dcd466372a6de62749762adbdf4
Marko Dodik <marko.dodik21@gmail.com>
2023-09-05 10:33:00 +0200

    Pattern3: Fast and slow pointers, init commit

---
+++ src/main/java/org/example/pattern3/Solution.java
@@ -0,0 +1,14 @@
+package org.example.pattern3;
+
+/**
+ * Pattern 3: Fast & Slow pointers
+ *
+ *  https://github.com/Chanda-Abdul/Several-Coding-Patterns-for-Solving-Data-Structures-and-Algorithms-Problems-during-Interviews/blob/main/%E2%9C%85%20%20Pattern%2003%3A%20Fast%20%26%20Slow%20pointers.md
+ *
+ * */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+}
