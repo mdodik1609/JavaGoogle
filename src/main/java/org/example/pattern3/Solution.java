8df529aa1344230434eb932cda0974877f71d751
Marko Dodik <marko.dodik21@gmail.com>
2023-09-05 13:00:11 +0200

    palindrome linked list

--- src/main/java/org/example/pattern3/Solution.java
+++ src/main/java/org/example/pattern3/Solution.java
@@ -12,15 +12,10 @@ public class Solution {
     public static void main(String[] args) {
         ListNode head = new ListNode(1);
         head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
-        head.next.next.next.next.next = new ListNode(6);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(1);
 
-        System.out.println(hasCycle(head));
-
-        head.next.next.next.next.next.next = head.next.next;
-        System.out.println(hasCycle(head));
+        System.out.println(isPalindrome(head));
 
 
     }
@@ -111,6 +106,40 @@ public class Solution {
         }
         return slow;
     }
+
+    /**
+     *  https://leetcode.com/problems/palindrome-linked-list/
+     *
+     *  234. Palindrome Linked list
+     * */
+     public static boolean isPalindrome(ListNode head) {
+         if(head == null) return true;
+         ListNode p1 = head;
+         ListNode p2 = head;
+         ListNode p3 = p1.next;
+         ListNode pre = p1;
+
+         while(p2.next != null && p2.next.next != null) {
+             p2 = p2.next.next;
+             pre = p1;
+             p1 = p3;
+             p3 = p3.next;
+             p1.next = pre;
+         }
+
+         if(p2.next == null) {
+             p1 = p1.next;
+         }
+
+         while(p3 != null) {
+             if(p1.val != p3.val) {
+                 return false;
+             }
+             p1 = p1.next;
+             p3 = p3.next;
+         }
+         return true;
+     }
 
 }
 
