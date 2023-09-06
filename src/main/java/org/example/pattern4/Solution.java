b3bc784dbc5dc02ccff1030e47e6733218136228
Marko Dodik <marko.dodik21@gmail.com>
2023-09-06 07:45:11 +0200

    merge intervals patter init commit

---
+++ src/main/java/org/example/pattern4/Solution.java
@@ -0,0 +1,13 @@
+package org.example.pattern4;
+
+public class Solution {
+    /**
+     *  https://github.com/Chanda-Abdul/Several-Coding-Patterns-for-Solving-Data-Structures-and-Algorithms-Problems-during-Interviews/blob/main/%E2%9C%85%20%20Pattern%2004%20%3A%20Merge%20Intervals.md
+     *
+     *  Pattern 4 : Merge Intervals
+     *
+     * */
+    public static void main(String[] args) {
+
+    }
+}
