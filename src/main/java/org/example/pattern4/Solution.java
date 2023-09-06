cefc3ae681f38d8022efce85461f8e9f12141101
Marko Dodik <marko.dodik21@gmail.com>
2023-09-06 09:27:26 +0200

    insert interval, but now optimizied without using merge function from task before

--- src/main/java/org/example/pattern4/Solution.java
+++ src/main/java/org/example/pattern4/Solution.java
@@ -71,6 +71,27 @@ public class Solution {
         return merge(result);
     }
 
+    public static int[][] insertOptimized(int[][] intervals, int[] interval) {
+        ArrayList<int[]> result = new ArrayList<>();
+        int i = 0;
+
+        while(i < intervals.length && intervals[i][1] < interval[0]) {
+            result.add(intervals[i]);
+            i++;
+        }
+        while(i < intervals.length && intervals[i][0] <= interval[1]) {
+            interval[0] = Math.min(intervals[i][0], interval[0]);
+            interval[1] = Math.max(intervals[i][1], interval[1]);
+            i++;
+        }
+        result.add(interval);
+        while(i < intervals.length) {
+            result.add(intervals[i]);
+            i++;
+        }
+        return result.toArray(new int[result.size()][]);
+    }
+
 }
 
 class Interval{
