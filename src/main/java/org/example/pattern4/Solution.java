edad441152178f51fc5994f209b3ee30bd5b082a
Marko Dodik <marko.dodik21@gmail.com>
2023-09-06 10:20:09 +0200

    minimal number of meeting rooms (Meeting Rooms II)

--- src/main/java/org/example/pattern4/Solution.java
+++ src/main/java/org/example/pattern4/Solution.java
@@ -1,9 +1,6 @@
 package org.example.pattern4;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
     /**
@@ -20,14 +17,32 @@ public class Solution {
 //                {1,4},{0,4}
 //        }));
 
+//        System.out.println(
+//                insert(
+//                        new int[][]{
+//                                {1,3}, {6,9}
+//                        },
+//                        new int[]{2,5}
+//                )
+//        );
+
         System.out.println(
-                insert(
+                minNumOfRooms(
                         new int[][]{
-                                {1,3}, {6,9}
-                        },
-                        new int[]{2,5}
+                                {1,4},{2,5},{7,9}
+                        }
                 )
         );
+        System.out.println(minNumOfRooms(
+                new int[][]{
+                        {1,4}
+                }
+        ));
+        System.out.println(minNumOfRooms(
+                new int[][]{
+                        {4,5},{2,3},{2,4},{3,5}
+                }
+        ));
     }
 
     /**
@@ -92,18 +107,82 @@ public class Solution {
         return result.toArray(new int[result.size()][]);
     }
 
+    /**
+     *  https://leetcode.com/problems/meeting-rooms-ii/
+     *
+     *  Meeting rooms II
+     * */
+    public static int minNumOfRooms(int[][] meetings) {
+        if(meetings.length == 0) return 0;
+        //ArrayList<ArrayList<int[]>> result = new ArrayList<>();
+
+        ArrayList<ArrayList<Interval>> result2 = new ArrayList<>();
+        result2.add(new ArrayList<>(Collections.singleton(
+            new Interval(meetings[0])
+        )));
+
+        //result.add(new ArrayList<>(Collections.singleton(meetings[0])));
+
+        Arrays.sort(meetings, Comparator.comparingInt(a -> a[0]));
+        // place meeting in a room if it is available
+        // place an interval to one of arrays where there will not be cross-sections
+        for(int i = 1; i < meetings.length; i++) {
+
+            for(int j = 0; j < result2.size(); j++) {
+                int finalI = i;
+                if(result2.get(j).stream().filter(
+                        it ->  it.checkOverlap(new Interval(meetings[finalI]))  //(meetings[finalI][0] > it.start && meetings[finalI][1] < it.end) || (meetings[finalI][0] > it.start && meetings[finalI][1] < it.end)
+                ).count() == 0) {
+                    var temp = result2.get(j);
+                    result2.remove(temp);
+                    temp.add(new Interval(meetings[finalI]));
+                    result2.add(temp);
+                } else {
+                    result2.add(new ArrayList<>(Collections.singleton(
+                            new Interval(meetings[finalI])
+                    )));
+                }
+            }
+        }
+
+        return result2.size();
+    }
+
 }
 
 class Interval{
-    int start;
-    int end;
-    Interval(int start, int end) {
+    public int start;
+    public int end;
+    public Interval(int start, int end) {
         this.start = start;
         this.end = end;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public Interval(int[] field) {
+        this.start = field[0];
+        this.end = field[1];
+    }
+
+    public boolean checkOverlap(Interval i2){
+        if(this.start > i2.start && this.end < i2.end) return true;
+        if(this.end > i2.start && this.end < i2.end) return true;
+        return false;
     }
 
     @Override
     public String toString() {
         return "[" + this.start + ", " + this.end + "]";
+    }
+
+    public boolean equals(Interval interval) {
+        return this.start == interval.start && this.end == interval.end;
     }
 }
