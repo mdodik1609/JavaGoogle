a53f71c9196cb740f3de2178c7ffdf53ad87c2d7
Marko Dodik <marko.dodik21@gmail.com>
2023-09-07 15:49:45 +0200

    [TREE BREADTH FIRST SEARCH] init commit

---
+++ src/main/java/org/example/pattern7/Solution.java
@@ -0,0 +1,13 @@
+package org.example.pattern7;
+
+public class Solution {
+    /**
+     *  https://github.com/Chanda-Abdul/Several-Coding-Patterns-for-Solving-Data-Structures-and-Algorithms-Problems-during-Interviews/blob/main/%E2%9C%85%20%20Pattern%2007:%20Tree%20Breadth%20First%20Search.md
+     *
+     *
+     *  Pattern 7: Tree Breadth First Search
+     * */
+    public static void main(String[] args) {
+
+    }
+}
