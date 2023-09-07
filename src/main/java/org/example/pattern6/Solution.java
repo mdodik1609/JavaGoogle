931c58874cf6436a5b45cb80474cdc3a88256b7a
Marko Dodik <marko.dodik21@gmail.com>
2023-09-07 12:56:31 +0200

    In-place reversal of a LinkedList init commit

---
+++ src/main/java/org/example/pattern6/Solution.java
@@ -0,0 +1,15 @@
+package org.example.pattern6;
+
+public class Solution {
+    /**
+     *      https://github.com/Chanda-Abdul/Several-Coding-Patterns-for-Solving-Data-Structures-and-Algorithms-Problems-during-Interviews/blob/main/%E2%9C%85%20%20Pattern%2006:%20In-place%20Reversal%20of%20a%20LinkedList.md
+     *
+     *      Pattern 6: In-place reversal of a LinkedList
+     * */
+    public static void main(String[] args){
+
+    }
+
+
+
+}
