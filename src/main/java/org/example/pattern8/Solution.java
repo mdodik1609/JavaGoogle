0b7e99f2cc31a22da6ef5125437443a1b5120473
Marko Dodik <marko.dodik21@gmail.com>
2023-09-09 23:30:26 +0200

    [Tree Depth First Search] init commit

---
+++ src/main/java/org/example/pattern8/Solution.java
@@ -0,0 +1,12 @@
+package org.example.pattern8;
+
+public class Solution {
+    /**
+     * https://github.com/Chanda-Abdul/Several-Coding-Patterns-for-Solving-Data-Structures-and-Algorithms-Problems-during-Interviews/blob/main/%E2%9C%85%20%20Pattern%2008:Tree%20Depth%20First%20Search.md
+     *
+     * Pattern 8: Tree Depth First Search (DFS)
+     * */
+    public static void main(String[] args) {
+
+    }
+}
