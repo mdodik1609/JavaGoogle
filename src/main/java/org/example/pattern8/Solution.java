2363bfb7c27ba0e853380fbae73f1be27c58475c
Marko Dodik <marko.dodik21@gmail.com>
2023-09-11 15:08:25 +0200

    path sum

--- src/main/java/org/example/pattern8/Solution.java
+++ src/main/java/org/example/pattern8/Solution.java
@@ -9,4 +9,35 @@ public class Solution {
     public static void main(String[] args) {
 
     }
+    /**
+     *  https://leetcode.com/problems/path-sum/
+     *
+     *  112. Path sum
+     * */
+    public static boolean hasPathSum(TreeNode root, int targetSum) {
+        if(root == null) return false;
+
+        int val = root.val;
+
+        if(val == targetSum && isLeaf(root)) return true;
+        return (hasPathSum(root.left, targetSum - val) || hasPathSum(root.right, targetSum - val));
+    }
+
+    public static boolean isLeaf(TreeNode node) {
+        if(node.left == null && node.right == null) return true;
+        return false;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
 }
