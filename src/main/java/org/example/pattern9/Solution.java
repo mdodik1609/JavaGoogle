e4f3c14591691bbfd4269e4b7883cb6899fdc13b
Marko Dodik <marko.dodik21@gmail.com>
2023-09-12 15:03:08 +0200

    sliding window median

--- src/main/java/org/example/pattern9/Solution.java
+++ src/main/java/org/example/pattern9/Solution.java
@@ -12,13 +12,38 @@ public class Solution {
      *
      * */
     public static void main(String[] args) {
-        MedianFinder finder = new MedianFinder();
-        finder.addNum(1);
-        finder.addNum(2);
-        finder.addNum(3);
-        System.out.println(finder.findMedian());
+//        MedianFinder finder = new MedianFinder();
+//        finder.addNum(1);
+//        finder.addNum(2);
+//        finder.addNum(3);
+//        System.out.println(finder.findMedian());
+
+        System.out.println(medianSlidingWindow(
+                new int[]{1,3,-1,-3,5,3,6,7}, 3
+        ));
     }
 
+    /**
+     *  https://leetcode.com/problems/sliding-window-median/
+     *
+     *  480. Sliding Window Median
+     * */
+    public static double[] medianSlidingWindow(int[] nums, int k) {
+        if(nums.length == 0) return new double[]{};
+        double[] result = new double[nums.length - k + 1];
+        int j = 0;
+        MedianFinder medianFinder = new MedianFinder();
+        for(int i = 0; i < k - 1; i++) {
+            medianFinder.addNum(nums[i]);
+        }
+        for(int i = k - 1; i < nums.length; i++) {
+            medianFinder.addNum(nums[i]);
+            result[j]= medianFinder.findMedian();
+            j++;
+            medianFinder.removeNumber(nums[i - k + 1]);
+        }
+        return result;
+    }
 }
 
 /**
@@ -33,6 +58,20 @@ class MedianFinder {
 
     public MedianFinder() {
 
+    }
+
+    public void balanceStack() {
+        if(maxStack.size() < minStack.size()) {
+            maxStack.add(minStack.poll());
+        }
+    }
+
+    public void removeNumber(int num) {
+        if(!maxStack.remove(num)){
+            minStack.remove(num);
+        } else {
+            balanceStack();
+        }
     }
 
     public void addNum(int num) {
