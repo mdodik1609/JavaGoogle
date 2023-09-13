fa0bf7c682fe42304e016dd0da9dd59774efc102
Marko Dodik <marko.dodik21@gmail.com>
2023-09-13 14:51:20 +0200

    [Modified Binary Search] init commit

---
+++ src/main/java/org/example/pattern11/Solution.java
@@ -0,0 +1,13 @@
+package org.example.pattern11;
+
+public class Solution {
+    /**
+     *  https://github.com/Chanda-Abdul/Several-Coding-Patterns-for-Solving-Data-Structures-and-Algorithms-Problems-during-Interviews/blob/main/%E2%9C%85%20%20Pattern%2011%3A%20Modified%20Binary%20Search.md
+     *
+     *  Pattern 11: Modified Binary Search
+     *
+     * */
+    public static void main(String[] args) {
+
+    }
+}
