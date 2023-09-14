b05142a1eb762cf90b2f185648dfca0b380a38fb
Marko Dodik <marko.dodik21@gmail.com>
2023-09-14 13:57:16 +0200

    next greates letter

--- src/main/java/org/example/pattern11/Solution.java
+++ src/main/java/org/example/pattern11/Solution.java
@@ -13,10 +13,14 @@ public class Solution {
 //                        new int[]{-1,0,3,5,7,9,12}, 9
 //                )
 //        );
+//        System.out.println(
+//                searchInsert(
+//                        new int[]{1,3,5,6}, 7
+//                )
+//        );
+
         System.out.println(
-                searchInsert(
-                        new int[]{1,3,5,6}, 7
-                )
+                nextGreatestLetter(new char[]{'c','f','j'}, 'd')
         );
     }
 
@@ -84,5 +88,24 @@ public class Solution {
             }
         }
         return end + 1;
+    }
+    /**
+     *  https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+     *
+     *  744. Find Smallest Letter Greater Than Target
+     * */
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+
+        while (start <= end) {
+            int mid = (int) Math.floor(start + (end - start) / 2);
+            if(target < letters[mid] ) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return letters[start % letters.length];
     }
 }
