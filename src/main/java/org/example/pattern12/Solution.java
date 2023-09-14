6f5077683eb33d123a28f89ebfd88486f4b9e4fb
Marko Dodik <marko.dodik21@gmail.com>
2023-09-14 19:40:52 +0200

    [Bitwise XOR] init commit

---
+++ src/main/java/org/example/pattern12/Solution.java
@@ -0,0 +1,12 @@
+package org.example.pattern12;
+
+public class Solution {
+    /**
+     *  https://github.com/Chanda-Abdul/Several-Coding-Patterns-for-Solving-Data-Structures-and-Algorithms-Problems-during-Interviews/blob/main/%E2%9C%85%20Pattern%2012%3A%20%20Bitwise%20XOR.md
+     *
+     *  Pattern 12: Bitwise XOR
+     * */
+    public static void main(String[] args) {
+
+    }
+}
