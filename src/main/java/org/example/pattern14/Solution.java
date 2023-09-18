44ec182086a7aa6c0edd1470c411d88d5af4c4a7
Marko Dodik <marko.dodik21@gmail.com>
2023-09-18 21:23:42 +0200

    [K-way merge] init commit

---
+++ src/main/java/org/example/pattern14/Solution.java
@@ -0,0 +1,12 @@
+package org.example.pattern14;
+
+public class Solution {
+    /**
+     * https://github.com/Chanda-Abdul/Several-Coding-Patterns-for-Solving-Data-Structures-and-Algorithms-Problems-during-Interviews/blob/main/%E2%9C%85%20Pattern%2014%3A%20K-way%20merge.md
+     *
+     *  Pattern 14: K-way merge
+     * */
+    public static void main(String[] args) {
+
+    }
+}
