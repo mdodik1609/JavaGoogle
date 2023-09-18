0371ab9b88ad81c0d9e5c8e47cb9591d0db13475
Marko Dodik <marko.dodik21@gmail.com>
2023-09-18 22:48:13 +0200

    Smallest Range Covering Elements from K Lists

--- src/main/java/org/example/pattern14/Solution.java
+++ src/main/java/org/example/pattern14/Solution.java
@@ -1,5 +1,6 @@
 package org.example.pattern14;
 
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class Solution {
@@ -57,6 +58,52 @@ public class Solution {
         }
 
         return  finalRes.next;
+    }
+
+    /**
+     *  https://leetcode.com/problems/smallest-range-covering-elements-from-k-lists/
+     *
+     *  632. Smallest Range Covering Elements from K Lists
+     * */
+    public int[] smallestRange(List<List<Integer>> nums) {
+        if(nums == null || nums.size() == 0) return new int[0];
+
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(
+                (a,b) -> a[2] - b[2]
+        );
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.size(); i++) {
+            int val = nums.get(i).get(0);
+            pq.add(new int[]{i, 0, val});
+            min = Math.min(min, val);
+            max = Math.max(max, val);
+        }
+
+        int start = min;
+        int end = max;
+        int minRange = end - start;
+        while(pq.size() == nums.size()) {
+            int[] q = pq.poll();
+            int row = q[0];
+            int col = q[1];
+            int val = q[2];
+
+            if(max - val < minRange) {
+                minRange = max - val;
+                min = val;
+                start = min;
+                end = max;
+            }
+
+            if(col + 1 < nums.get(row).size()) {
+                int newVal = nums.get(row).get(col + 1);
+                pq.add(new int[]{row, col + 1, newVal});
+                max = Math.max(max, newVal);
+            }
+        }
+        return new int[]{start, end};
     }
 
 }
