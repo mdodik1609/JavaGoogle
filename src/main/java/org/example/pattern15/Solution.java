9cca84467a1fbce3336951fec8814b95cb6cf6b7
Marko Dodik <marko.dodik21@gmail.com>
2023-09-19 16:16:02 +0200

    house robber

--- src/main/java/org/example/pattern15/Solution.java
+++ src/main/java/org/example/pattern15/Solution.java
@@ -185,4 +185,25 @@ public class Solution {
         }
         return dp[n-1];
     }
+
+    /**
+     *  https://leetcode.com/problems/house-robber
+     *
+     *  198. House Robber
+     * */
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+
+        for(int i = 1; i < n; i ++) {
+            dp[i + 1] = Math.max(
+                    dp[i-1] + nums[i],
+                    dp[i]
+            );
+        }
+        return dp[n];
+    }
 }
