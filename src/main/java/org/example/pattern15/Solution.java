fa0288d2cf37acd8a55375d5258706e4cb26ba90
Marko Dodik <marko.dodik21@gmail.com>
2023-09-19 17:34:19 +0200

    longest common subsequence

--- src/main/java/org/example/pattern15/Solution.java
+++ src/main/java/org/example/pattern15/Solution.java
@@ -233,4 +233,35 @@ public class Solution {
         }
         return s.length() - dp[0][n-1];
     }
+
+    /**
+     *  https://leetcode.com/problems/longest-common-subsequence
+     *
+     *  1143. Longest Common Subsequence
+     * */
+    public int longestCommonSubsequence(String text1, String text2) {
+        int n1 = text1.length();
+        int n2 = text2.length();
+        int result = 0;
+
+        int[][] dp = new int[n1+1][n2+1];
+
+        for(int i = 1; i <= n1; i++) {
+            for(int j = 1; j <= n2; j++) {
+                if(text1.charAt(i-1) == text2.charAt(j-1)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.max(
+                            dp[i-1][j],
+                            dp[i][j-1]
+                    );
+                }
+                result = Math.max(
+                        result,
+                        dp[i][j]
+                );
+            }
+        }
+        return result;
+    }
 }
