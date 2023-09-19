4cfd4d92daf30116f402c39d0a3a3e59f6f017f8
Marko Dodik <marko.dodik21@gmail.com>
2023-09-20 00:54:26 +0200

    min abs diff server load

--- src/main/java/org/example/randomTasks/Solution.java
+++ src/main/java/org/example/randomTasks/Solution.java
@@ -1,5 +1,7 @@
 package org.example.randomTasks;
 
+import java.util.Arrays;
+
 public class Solution {
     /**
      * Random tasks from here:
@@ -7,6 +9,29 @@ public class Solution {
      *
      * */
     public static void main(String[] args) {
+        System.out.println(
+                minAbs(
+                        new int[]{1,2,3,4,5}
+                )
+        );
+    }
 
+    /**
+     *  https://leetcode.com/discuss/interview-question/356433/
+     *
+     * */
+    public static int minAbs(int[] tasks){
+        int sum = Arrays.stream(tasks).sum();
+        int halfSum = sum / 2;
+        int[] dp = new int[halfSum + 1];
+
+        for(int i = 0; i < tasks.length; i++) {
+            if(dp[halfSum] == halfSum) break;
+            if(tasks[i] > halfSum) continue;
+            for(int j = halfSum; j >= tasks[i]; j--) {
+                dp[j] = Math.max(dp[j], tasks[i] + dp[j - tasks[i]]);
+            }
+        }
+        return (sum - dp[halfSum]) - dp[halfSum];
     }
 }
