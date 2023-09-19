3fc582fe361341ae37b92d734cca8bcb6d957bf6
Marko Dodik <marko.dodik21@gmail.com>
2023-09-20 00:06:58 +0200

    number of good ways to split a string

--- src/main/java/org/example/splitString/Main.java
+++ src/main/java/org/example/splitString/Main.java
@@ -1,6 +1,7 @@
 package org.example.splitString;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -49,5 +50,38 @@ public class Main {
                 seg23++;
         }
         return (int)((seg12*seg23)%modulo);
+    }
+
+    /**
+     *  https://leetcode.com/problems/number-of-good-ways-to-split-a-string/description/
+     *
+     *  1525. Number of Good Ways to Split a String
+     * */
+    public int numSplits(String s) {
+        HashMap<Character, Integer> right = new HashMap<Character, Integer>();
+
+        for(int i = 0; i < s.length();i++) {
+            right.put(s.charAt(i), right.getOrDefault(s.charAt(i), 0) + 1);
+            // if(left.get(s.charAt(i)) == null) left.add(s.chartAt(i), 0);
+            // left.replace(s.charAt(i), left.get(s.chartAt(i)));
+        }
+
+        HashMap<Character, Integer> left = new HashMap<Character, Integer>();
+        int countGoodSplit = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            left.put(s.charAt(i), left.getOrDefault(s.charAt(i), 0) + 1);
+
+            right.put(s.charAt(i), right.get(s.charAt(i)) - 1);
+            if(right.get(s.charAt(i)) == 0) {
+                right.remove(s.charAt(i));
+            }
+
+            if(left.size() == right.size()) {
+                countGoodSplit++;
+            }
+        }
+
+        return countGoodSplit;
     }
 }
