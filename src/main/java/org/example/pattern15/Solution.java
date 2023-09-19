1a7c928fbe164f5bac1d7b8207c479b427818706
Marko Dodik <marko.dodik21@gmail.com>
2023-09-19 13:16:09 +0200

    target sum

--- src/main/java/org/example/pattern15/Solution.java
+++ src/main/java/org/example/pattern15/Solution.java
@@ -1,6 +1,7 @@
 package org.example.pattern15;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
@@ -38,5 +39,38 @@ public class Solution {
         }
 
         return dp[target] == null ? new ArrayList<>() : dp[target];
+    }
+
+    /**
+     *  https://leetcode.com/problems/target-sum/
+     *
+     *  494. Target sum
+     * */
+    public static int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
+
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+
+        if(sum - target < 0 || (sum - target) % 2 == 1) {
+            return 0;
+        }
+
+        int halfSum = (sum - target) / 2;
+        int[][] dp = new int[n + 1][halfSum + 1];
+        dp[0][0] = 1;
+
+        for(int i = 1; i < n + 1; i++) {
+            for(int j = 0; j <= halfSum; j++) {
+                if(nums[i - 1] <= j) {
+                    dp[i][j] = dp[i-1][j] + dp[i-1][j - nums[i - 1]];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][halfSum];
     }
 }
