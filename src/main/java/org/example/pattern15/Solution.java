5be5a54936cca704dd52b57f09c4e02d95b5f055
Marko Dodik <marko.dodik21@gmail.com>
2023-09-19 15:44:00 +0200

    fibonacci number

--- src/main/java/org/example/pattern15/Solution.java
+++ src/main/java/org/example/pattern15/Solution.java
@@ -146,4 +146,23 @@ public class Solution {
         if(dp[n-1][amount] > 10000) return -1;
         return dp[n - 1][amount];
     }
+    /**
+     *  https://leetcode.com/problems/fibonacci-number/
+     *
+     *  509. Fibonacci number
+     * */
+    public int fib(int n) {
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+
+        int[] dp = new int[n+1];
+
+        dp[0] = 0;
+        dp[1] = dp[2] = 1;
+
+        for(int i = 3; i <= n; i++) {
+            dp[i] = dp[i -1] + dp[i -2];
+        }
+        return dp[n];
+    }
 }
