8c1201258fafc1483b5442e49fe8ca32840eea52
Marko Dodik <marko.dodik21@gmail.com>
2023-09-19 14:02:32 +0200

    minimum cost to cut a stick

--- src/main/java/org/example/pattern15/Solution.java
+++ src/main/java/org/example/pattern15/Solution.java
@@ -7,8 +7,11 @@ import java.util.List;
 public class Solution {
 
     public static void main(String[] args) {
-        combinationSum(
-                new int[]{2,3,7}, 7
+//        combinationSum(
+//                new int[]{2,3,7}, 7
+//        );
+        minCost(
+                9, new int[]{5,6,1,4,7}
         );
     }
 
@@ -72,5 +75,37 @@ public class Solution {
             }
         }
         return dp[n][halfSum];
+    }
+
+    /**
+     *      https://leetcode.com/problems/minimum-cost-to-cut-a-stick/
+     *
+     *  1547. Minimum Cost to Cut a Stick
+     * */
+    public static int minCost(int n, int[] cuts) {
+        Arrays.sort(cuts);
+        int numOfCuts = cuts.length;
+        int[] cuts2 = new int[numOfCuts + 2];
+        cuts2[0] = 0;
+        for(int i = 1; i < cuts2.length - 1; i++) {
+            cuts2[i] = cuts[i - 1];
+        }
+        cuts2[numOfCuts + 1] = n;
+
+        int[][] dp = new int[numOfCuts + 2][numOfCuts + 2];
+
+        for(int i = 2; i < numOfCuts + 2; i++) {
+            for(int j = 0; j < numOfCuts + 2 - i; j++) {
+                int k = j + i;
+                dp[j][k] = Integer.MAX_VALUE;
+                for(int l = j + 1; l < k; l++){
+                    dp[j][k] = Math.min(
+                            dp[j][k],
+                            dp[j][l] + dp[l][k] + cuts2[k] - cuts2[j]
+                    );
+                }
+            }
+        }
+        return dp[0][numOfCuts + 1];
     }
 }
