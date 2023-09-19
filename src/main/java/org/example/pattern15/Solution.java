c1be40f9820aac4484d3fef008389b4f5e6a34c2
Marko Dodik <marko.dodik21@gmail.com>
2023-09-19 18:12:36 +0200

    edit distance

--- src/main/java/org/example/pattern15/Solution.java
+++ src/main/java/org/example/pattern15/Solution.java
@@ -276,10 +276,10 @@ public class Solution {
 
         int result = 1;
 
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             dp[i] = 1;
-            for(int j = 0; j < i; j++) {
-                if(nums[i] > nums[j] && dp[i] <= dp[j]) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j] && dp[i] <= dp[j]) {
                     dp[i] = dp[j] + 1;
                     result = Math.max(
                             result, dp[i]
@@ -289,5 +289,39 @@ public class Solution {
 
         }
         return result;
+    }
+
+    /**
+     *  https://leetcode.com/problems/edit-distance/description/
+     *
+     *  72. Edit distance
+     **/
+    public int minDistance(String word1, String word2) {
+        int n1 = word1.length();
+        int n2 = word2.length();
+        int[][] dp = new int[n1+1][n2+1];
+        for(int i = 0; i < n1 + 1; i++) {
+            dp[i][0] = i;
+        }
+        for(int i = 0; i < n2 + 1; i++) {
+            dp[0][i] = i;
+        }
+
+        for(int i = 0; i < n1; i++) {
+            for(int j = 0; j < n2; j++) {
+                if(word1.charAt(i) == word2.charAt(j)) {
+                    dp[i+1][j+1] = dp[i][j];
+                } else {
+                    dp[i+1][j+1] = 1 + Math.min(
+                            Math.min(
+                                    dp[i][j+1],
+                                    dp[i+1][j]
+                            ),
+                            dp[i][j]
+                    );
+                }
+            }
+        }
+        return dp[n1][n2];
     }
 }
