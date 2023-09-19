19b71113773e0abe4dc15893c7565a5412c76265
Marko Dodik <marko.dodik21@gmail.com>
2023-09-19 17:51:00 +0200

    longest increasing subsequence

--- src/main/java/org/example/pattern15/Solution.java
+++ src/main/java/org/example/pattern15/Solution.java
@@ -264,4 +264,30 @@ public class Solution {
         }
         return result;
     }
+    /**
+     *  https://leetcode.com/problems/longest-increasing-subsequence/
+     *
+     *  300. Longest Increasing Subsequence
+     * */
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+
+        int[] dp = new int[n];
+
+        int result = 1;
+
+        for(int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for(int j = 0; j < i; j++) {
+                if(nums[i] > nums[j] && dp[i] <= dp[j]) {
+                    dp[i] = dp[j] + 1;
+                    result = Math.max(
+                            result, dp[i]
+                    );
+                }
+            }
+
+        }
+        return result;
+    }
 }
