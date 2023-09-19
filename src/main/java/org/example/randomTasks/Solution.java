ad265f0e08f083eb08fc47988dc4a398f264e28f
Marko Dodik <marko.dodik21@gmail.com>
2023-09-20 00:41:11 +0200

    radnom tasks init commit

---
+++ src/main/java/org/example/randomTasks/Solution.java
@@ -0,0 +1,12 @@
+package org.example.randomTasks;
+
+public class Solution {
+    /**
+     * Random tasks from here:
+     *  https://leetcode.com/discuss/interview-question/352460/Google-Online-Assessment-Questions
+     *
+     * */
+    public static void main(String[] args) {
+
+    }
+}
